5c7c2d7b9b8c1769564350b911dc343ae644e976
Mateus Henrique Tofanello <mateus09h@gmail.com>
2019-10-14 12:33:03 -0300

    Form

--- models/Lead.js
+++ models/Lead.js
@@ -1,19 +1,23 @@
 const firebase = require('../firebase');
 const stringify = require('csv-stringify');
 
-const create = ({ name }) => {
+const create = ({ name, empresa, cargo, email, ajuda }) => {
     const leads = firebase.database().ref('leads');
-    const lead = leads.push({ name });
+    const lead = leads.push({ name, empresa, cargo, email, ajuda });
     return lead;
 };
 
 const csv = (cb) => {
     const leads = firebase.database().ref('leads');
-    const data = [['id', 'name']];
+    const data = [['id', 'name', 'empresa', 'cargo', 'email', 'ajuda']];
     leads.on('value', (snapshot) => {
         snapshot.forEach((lead) => {
             const { name } = lead.val();
-            data.push([lead.key, name]);
+            const { empresa } = lead.val();
+            const { cargo } = lead.val();
+            const { email } = lead.val();
+            const { ajuda } = lead.val();
+            data.push([lead.key, name, empresa, cargo, email, ajuda]);
         });
         stringify(data, (err, output) => {
             cb(output);
